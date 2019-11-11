c4242bbf8b3546c028e1660003354478f57fc7a9
OliverGrob <groboliver1117@gmail.com>
2019-11-11 22:01:05 +0100

    add new expense placeholder constant

--- app/src/main/java/com/ogrob/moneybox/utils/Constants.kt
+++ app/src/main/java/com/ogrob/moneybox/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.ogrob.moneybox.utils
 
-const val NO_CATEGORY_ID = 1
+const val NEW_EXPENSE_PLACEHOLDER_ID = -1L
+
+const val NO_CATEGORY_ID = 1L
 const val NO_CATEGORY_DISPLAY_TEXT = "No Category"
 
 const val EMPTY_STRING = ""
